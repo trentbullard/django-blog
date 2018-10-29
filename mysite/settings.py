ec12cfe6f5a436aef1d53cf3704d9f85d7a9adc7
trent bullard <trent.bullard@dhsgroup.com>
2018-10-29 17:17:11 -0500

    fix db config

--- mysite/settings.py
+++ mysite/settings.py
@@ -90,14 +90,6 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# if DEBUG:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         }
-#     }
-# else:
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
